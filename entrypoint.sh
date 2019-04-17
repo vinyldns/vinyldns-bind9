e7ce633aac9f10f7be6e393d0c3dda10ab57b063
Rebecca Star <rebecca_star@comcast.com>
2019-04-17 16:59:56 -0400

    revert switch to debian

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,16 @@
 set -e
 
 echo "Copying zone files to /var/bind"
-mkdir -p /var/bind
-cp -a /var/cache/bind/zones/. /var/bind/
+cp -rf /var/cache/bind/zones/* /var/bind
 
 echo "Copying over named.conf.local"
 cp -rf /var/cache/bind/config/named.conf.local /etc/bind
 
 echo "Changing owner to named user"
-mkdir -m 0775 -p /var/run/named
-chown -R bind:bind /var/bind /etc/bind /var/run/named /var/log/named
+chown -R named:named /var/bind /etc/bind /var/run/named /var/log/named
 chmod -R o-rwx /var/bind /etc/bind /var/run/named /var/log/named
 
 START_UP=$(which named)
-echo "Starting bind at $START_UP -u bind -c /etc/bind/named.conf -f"
+echo "Starting bind at $START_UP -u named -c /etc/bind/named.conf -f"
 
-exec $(which named) -u bind -c /etc/bind/named.conf -f
+exec $(which named) -u named -c /etc/bind/named.conf -f
